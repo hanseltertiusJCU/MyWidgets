ccc41423767bfe57194f6e112d611789dd89ed99
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-02-27 20:09:11 +0700

    Implement Action from Button to Update Widget

--- app/src/main/java/com/example/mywidgets/RandomNumbersWidget.java
+++ app/src/main/java/com/example/mywidgets/RandomNumbersWidget.java
@@ -1,8 +1,10 @@
 package com.example.mywidgets;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
@@ -10,7 +12,7 @@ import android.widget.RemoteViews;
  */
 public class RandomNumbersWidget extends AppWidgetProvider {
 	
-	static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+	void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
 								int appWidgetId) {
 		
 		// Initiate text
@@ -19,12 +21,13 @@ public class RandomNumbersWidget extends AppWidgetProvider {
 		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.random_numbers_widget);
 		// Set remoteviews text view text content from TextView id in Widget context
 		views.setTextViewText(R.id.appwidget_text, lastUpdate); // Parameternya adalah : id xml dr widget sm text dr String value
-		
+		// Set pending intent ke Button di Widget pada saat OnClick event
+		views.setOnClickPendingIntent(R.id.btn_click, getPendingSelfIntent(context, appWidgetId, WIDGET_CLICK));
 		// Instruct the widget manager to update the widget
 		appWidgetManager.updateAppWidget(appWidgetId, views);
 	}
 	
-	// Method tsb brguna ktika widget pertama kali dibuat atau dijalankan ketika updatePeriod
+	// Method tsb brguna ktika widget pertama kali dibuat atau dijalankan ketika updatePeriod yg ad di Widget xml
 	// (min. 30 mins) telah lewat
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -43,5 +46,32 @@ public class RandomNumbersWidget extends AppWidgetProvider {
 	public void onDisabled(Context context) {
 		// Enter relevant functionality for when the last widget is disabled
 	}
+	
+	private static String WIDGET_CLICK = "widgetsclick";
+	
+	// Method tsb berguna untuk menerima hasil dari broadcast oleh PendingIntent
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+		if(WIDGET_CLICK.equals(intent.getAction())){ // Cek jika static WIDGET_CLICK itu sama kyk action dari Intent di PendingIntent
+			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.random_numbers_widget); // Buat view widget
+			String lastUpdate = "Random: " + NumberGenerator.Generate(100);
+			int appWidgetId = intent.getIntExtra(WIDGET_ID_EXTRA, 0); // Dptin value appWidgetId dari Intent data
+			views.setTextViewText(R.id.appwidget_text, lastUpdate); // Update text view di random_numbers_widget xml (bawaan dari RemoteViews)
+			appWidgetManager.updateAppWidget(appWidgetId, views); // Update widget
+		}
+	}
+	
+	private static String WIDGET_ID_EXTRA = "widget_id_extra";
+	
+	protected PendingIntent getPendingSelfIntent(Context context, int appWidgetId, String action){
+		Intent intent = new Intent(context, getClass());
+		intent.setAction(action); // Set action dari intent bedasarkan parameter action
+		intent.putExtra(WIDGET_ID_EXTRA, appWidgetId); // Bawa data appWidgetId (bawaan dari updateAppWidget method) ke Intent
+		return PendingIntent.getBroadcast(context, appWidgetId, intent, 0); // Lakukan broadcast di PendingIntent
+	}
+	
+	
 }
 
